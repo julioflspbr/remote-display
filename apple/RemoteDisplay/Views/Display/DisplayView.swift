1ff41f0c0c05b3a33b02a6803dd039e807f98bb6
Júlio César Flores <julioflspbr@gmail.com>
2026-09-09 09:10:31 +0200

    Adjust the keyboard flow on Apple stack

--- apple/RemoteDisplay/Views/Display/DisplayView.swift
+++ apple/RemoteDisplay/Views/Display/DisplayView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DisplayView: View {
 	let text: String
-	@State private var viewModel = DisplayViewModel(dependencies: .live())
+	@State private var viewModel = DisplayViewModel()
 
 	var body: some View {
 		DisplayTextView(display: viewModel.display)
