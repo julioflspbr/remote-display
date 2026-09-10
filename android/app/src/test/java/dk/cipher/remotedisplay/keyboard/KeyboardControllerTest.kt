4b324a54fc1510e06e79e9fbe2720f077eca8008
Júlio César Flores <julioflspbr@gmail.com>
2026-09-10 08:25:47 +0200

    Re-write tests to accomodate the new Keybard system on Android stack

--- android/app/src/test/java/dk/cipher/remotedisplay/keyboard/KeyboardControllerTest.kt
+++ android/app/src/test/java/dk/cipher/remotedisplay/keyboard/KeyboardControllerTest.kt
@@ -1,70 +1,102 @@
 package dk.cipher.remotedisplay.keyboard
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Semaphore
-import kotlinx.coroutines.test.runTest
-
+import dk.cipher.remotedisplay.keyboard.Keyboard.Action
 import org.junit.Test
-import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertEquals
 
 class KeyboardControllerTest {
     @Test
-    fun `test keyboard input`() = runTest {
+    fun `toggle keyboard when controller allows it`() {
         // given
-        val semaphore = Semaphore(1)
+        val service = MockKeyboardService(false)
         val sut = KeyboardController()
+        sut.setService(service)
 
         // when
-        val collector = makeCollector(sut, semaphore)
-        semaphore.acquire()
-        CoroutineScope(Dispatchers.Default).launch {
-            sut.insertText("ab")
-            sut.insertText("c")
-            sut.insertText("")
-            sut.endChannel()
-        }
-
+        sut.canShowKeyboard = true
+        sut.toggleKeyboard()
         // then
-        val result = collector.await()
-        assertEquals("There were 2 valid text inputs, but the result does not contain 2 results", result.size, 2)
-        assertEquals("The first input doesn't match the output", result[0], KeyboardAction.Text("ab"))
-        assertEquals("The first input doesn't match the output", result[1], KeyboardAction.Text("c"))
+        assertTrue("The keyboard should be visible on first toggle", service.isKeyboardVisible)
+
+        // when
+        sut.toggleKeyboard()
+        // then
+        assertFalse("The keyboard should be hidden on second toggle", service.isKeyboardVisible)
     }
 
     @Test
-    fun `test backspace`() = runTest {
+    fun `controller does not allow keyboard toggling`() {
         // given
-        val semaphore = Semaphore(1)
+        val service = MockKeyboardService(false)
         val sut = KeyboardController()
+        sut.setService(service)
 
         // when
-        val collector = makeCollector(sut, semaphore)
-        semaphore.acquire()
-        CoroutineScope(Dispatchers.Default).launch {
-            sut.deleteBackward()
-            sut.deleteBackward()
-            sut.deleteBackward()
-            sut.endChannel()
-        }
+        sut.canShowKeyboard = false
+        sut.toggleKeyboard()
 
         // then
-        val result = collector.await()
-        assertEquals("There were 3 valid backspace inputs, but the result does not contain 3 results", result.size, 3)
-        assertTrue("Not all the inputs were backspaces", result.all { it == KeyboardAction.Backspace })
+        assertFalse("The keyboard should not be toggled when not allowed", service.isKeyboardVisible)
     }
 
-    private fun makeCollector(sut: KeyboardController, semaphore: Semaphore): Deferred<List<KeyboardAction>> =
-        CoroutineScope(Dispatchers.IO).async {
-            val result = mutableListOf<KeyboardAction>()
-            semaphore.release()
-            for (action in sut.keyboardAction) {
-                result.add(action)
-            }
-            return@async result
+    @Test
+    fun `canShoKeyboard automatically hides the keyboard when set to false`() {
+        // given
+        val service = MockKeyboardService(true)
+        val sut = KeyboardController()
+        sut.setService(service)
+
+        // when
+        sut.canShowKeyboard = true
+        sut.toggleKeyboard()
+        // then
+        assertTrue("The keyboard should be visible on first toggle", service.isKeyboardVisible)
+
+        // when
+        sut.canShowKeyboard = false
+        assertFalse("The keyboard should be hidden when canShowKeyboard is set to false", service.isKeyboardVisible)
+    }
+
+    @Test
+    fun `multiple subscribers receive the same actions and in sequence`() {
+        // given
+        val sut = KeyboardController()
+        val client1 = MockKeyboardClient()
+        val client2 = MockKeyboardClient()
+
+        // when
+        sut.subscribe(client1)
+        sut.subscribe(client2)
+
+        sut.insertText("ab")
+        sut.insertText("c")
+        sut.deleteBackward()
+        sut.insertText("")
+        sut.deleteBackward()
+
+        // then
+        val expectedActions = listOf(Action.Text("ab"), Action.Text("c"), Action.Backspace, Action.Backspace)
+        assertEquals("Client 1 should receive the same actions in the same order", expectedActions, client1.actions)
+        assertEquals("Client 2 should receive the same actions in the same order", expectedActions, client2.actions)
+    }
+
+    private class MockKeyboardService(var isKeyboardVisible: Boolean): Keyboard.Service {
+        override fun showKeyboard() {
+            this.isKeyboardVisible = true
         }
+
+        override fun hideKeyboard() {
+            this.isKeyboardVisible = false
+        }
+    }
+
+    private class MockKeyboardClient: Keyboard.Client {
+        var actions = mutableListOf<Action>()
+
+        override fun receive(action: Action) {
+            actions.add(action)
+        }
+    }
 }
